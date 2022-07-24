ecdd0dfb6409e2b7e17a87c70af61a3b7ffaffb4
TwentyEighty <TwentyEighty8020@gmail.com>
2022-07-24 16:01:07 -0400

    Improve collision mask to not touch everything

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,15 +2,14 @@ local collision_mask_util = require "collision-mask-util"
 local collision_data = require("prototypes.collision-mask")
 local flib_table = require("__flib__.table")
 
--- Add our custom collision mask to every object-like entity, except for straight rails,
--- which we need to build on
-for _,entity in pairs(collision_mask_util.collect_prototypes_with_layer("object-layer")) do
-    if entity.name ~= "straight-rail" then
-        entity.collision_mask = flib_table.deep_copy(collision_mask_util.get_mask(entity))
-        collision_mask_util.add_layer(entity.collision_mask, collision_data.layer)
-    end
-end
+local straight_rail = data.raw["straight-rail"]["straight-rail"]
+local curved_rail = data.raw["curved-rail"]["curved-rail"]
 
--- Adjust straight rails selection priority so that we can't x-ray select them through the
+-- Add collision mask to curved rails to prevent building on them
+curved_rail.collision_mask = flib_table.deep_copy(collision_mask_util.get_mask(curved_rail))
+collision_mask_util.add_layer(curved_rail.collision_mask, collision_data.layer)
+
+-- Adjust rails selection priority so that we can't x-ray select them through the
 -- train factory building, only x-ray select the locomotive
-data.raw["straight-rail"]["straight-rail"].selection_priority = 49
+straight_rail.selection_priority = 49
+curved_rail.selection_priority = 49
